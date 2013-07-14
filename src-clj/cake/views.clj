e469fcfad556b6773998e38251d35dffd2301a2f
Jake Stothard <stothardj@gmail.com>
2013-07-13 20:47:48 -0700

    Mess with layout. Add build menu which does nothing

--- src-clj/cake/views.clj
+++ src-clj/cake/views.clj
@@ -5,14 +5,23 @@
     [page :refer [include-js]]
     [page :refer [include-css]]]))
 
+(defn- menu [header & options]
+  (list
+   [:h2 header]
+   (map (fn [option] [:a {:href "#"} [:li option]]) options)))
+
 (defn index-page []
   (html5
    [:head
     (include-css "/css/style.css")
     [:title "OpenTD"]]
    [:body
-    [:div#container
-     [:h1 "OpenTD"]
-     [:canvas#game {:width "800" :height "600"}]]
+    [:div#full-page.cf
+     [:div#center-container
+      [:h1 "OpenTD"]
+      [:canvas#game {:width "800" :height "600"}]]
+     [:div#right-container
+      (menu "Build" "Laser Tower" "Charge Tower")]]
     (include-js "/js/main.js")
     ]))
+
