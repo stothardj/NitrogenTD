96bf95dac550e6523eb0a775154e1f3524c13249
Jake Stothard <stothardj@gmail.com>
2013-07-20 15:14:02 -0700

    First option auto selected in menu

--- src-clj/cake/views.clj
+++ src-clj/cake/views.clj
@@ -8,13 +8,21 @@
 (defn- menu [header & options]
   (list
    [:h2 header]
-   [:form {:id (str "menu-" header) :action ""}
-    (map (fn [option]
-           (list [:input {:type "radio" :name header :value option :id option}]
-                 [:label {:for option} option]
-                 [:br]))
-         options)]))
-
+   (when options
+     (letfn [(first-option [option]
+               (list [:input {:type "radio" :name header :value option :id option :checked true}]
+                     [:label {:for option} option]
+                     [:br]))
+             (rest-options [option]
+               (list [:input {:type "radio" :name header :value option :id option}]
+                     [:label {:for option} option]
+                     [:br]))]
+       (let [[f & r] options]
+         [:form {:id (str "menu-" header) :action ""}]
+         (cons
+          (first-option f)
+          (map rest-options r)))))))
+        
 (defn index-page []
   (html5
    [:head
