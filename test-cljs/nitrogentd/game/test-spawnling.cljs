ad6ab25282af40ee8c7132ea0f4c88a443159892
Jake Stothard <stothardj@gmail.com>
2013-09-08 14:18:21 -0700

    Test spawnling point and damage

--- test-cljs/nitrogentd/game/test-spawnling.cljs
+++ test-cljs/nitrogentd/game/test-spawnling.cljs
@@ -1,8 +1,27 @@
 (ns nitrogentd.game.test-spawnling
   (:require-macros [cemerick.cljs.test :refer (is deftest run-tests)])
+  (:use [nitrogentd.game.numberanimation :only [NumberAnimation]])
   (:require [cemerick.cljs.test :as t]
-            [nitrogentd.game.spawnling :as s]))
+            [nitrogentd.game.creep :as c]
+            [nitrogentd.game.spawnling :as s]
+            [nitrogentd.game.point :as p]))
 
 (deftest value-type
   (is (= (s/spawn-spawnling 1 2 nil) (s/spawn-spawnling 1 2 nil)))
   (is (not= (s/spawn-spawnling 1 2 nil) (s/spawn-spawnling 1 3 nil))))
+
+(deftest point
+  (is (= [5 7] (p/get-point (s/spawn-spawnling 5 7 nil)))))
+
+(deftest damage
+  (let [creep (s/spawn-spawnling 1 2 nil)
+        force 7 ;; Not enough to kill a spawnling
+        {:keys [creeps animations]} (c/damage creep force)]
+    (is (= 1 (count creeps)))
+    (is (= 1 (count animations)))
+    (is (= force (- (:health creep) (:health (first creeps)))))
+    (is (instance? NumberAnimation (first animations))))
+  (let [creep (s/spawn-spawnling 1 2 nil)
+        force (:health s/stats)
+        {:keys [creeps]} (c/damage creep force)]
+    (is (empty? creeps))))
