e01be2011a55d7ffba484164059a563f60a531d1
Jake Stothard <stothardj@gmail.com>
2013-10-28 00:28:53 -0700

    Fix glitch. Now wraps correctly

--- src-cljs/nitrogentd/game/levelinfo.cljs
+++ src-cljs/nitrogentd/game/levelinfo.cljs
@@ -7,16 +7,17 @@
   "Takes the current level info, not as an atom.
    Returns the corresponding level-info for the next thing."
   [info]
-  (cond (:waves info)  (assoc info :waves (rest (:waves info)))
-        (:levels info) {:levels (rest (:levels info))
-                        :waves (:waves (second (:level info)))}))
+  (cond (not-empty (:waves info))  (assoc info :waves (rest (:waves info)))
+        (not-empty (:levels info)) (let [next-levels (rest (:levels info))
+                                         next-waves (:waves (first next-levels))]
+                                     (when-not (and (empty? next-levels) (empty? next-waves))
+                                       {:levels next-levels
+                                        :waves next-waves}))))
 
 (defn load
   "Sets atoms to starting values defined by the level info"
   [info path pools]
   (let [level (first (:levels info))
         wave (first (:waves info))]
-    (.log js/console "Level" (clj->js level))
-    (.log js/console "Wave" (clj->js wave))
     (reset! path (:path level))
     (reset! pools (:pools wave))))
